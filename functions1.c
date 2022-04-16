9f476860380706e53b74243a17beff2672fc9025
ShagizMan <danielgetaneh2011@gmail.com>
2022-04-17 00:44:11 +0300

    #2 specifier function

---
+++ functions1.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_b - Custom conversion specifiers.
+ *
+ * @args: The number of arguments.
+ *
+ * Return: The unsigned int argument is converted to binary.
+ */
+int print_b(va_list args)
+{
+}
+
+/**
+ * print_u - Prints u.
+ *
+ * @args: The number of arguments
+ *
+ * Return: The number specified by u.
+ */
+int print_u(va_list args)
+{
+}
+
+/**
+ * print_o - Prints o.
+ *
+ * @args: The number of arguments.
+ *
+ * Return: The speciefer of o.
+ */
+int print_o(va_list args)
+{
+}
+
+/**
+ * print_x - Prints x.
+ *
+ * @args: The number of arguments.
+ *
+ * Return: The specifier that is denoted by x.
+ */
+int print_x(va_list args)
+{
+}
+
+/**
+ * print_X - Prints X.
+ *
+ * @args: The number of arguments.
+ *
+ * Return: The specifier that is denoted by X.
+ */
+int print_X(va_list args)
+{
+}
