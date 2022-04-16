9cfae5e6de70753f86eaff043b85df0362dcfc86
ShagizMan <danielgetaneh2011@gmail.com>
2022-04-17 00:04:14 +0300

    fix betty code

--- functions.c
+++ functions.c
@@ -27,22 +27,20 @@ int print_c(va_list args)
  */
 int print_s(va_list args)
 {
-        int index = 0, count = 0;
-        char *str;
+	int index = 0, count = 0;
+	char *str = va_arg(args, char *);
 
-        str = va_arg(args, char*);
+	if (str == NULL)
+		str = "(null)";
 
-        if (str == NULL)
-                str = "(null)";
+	while (str[index] != '\0')
+	{
+		_putchar(str[index]);
+		index++;
+		count++;
+	}
 
-        while (str[index] != '\0')
-        {
-                _putchar(str[index]);
-                index++;
-                count++;
-        }
-
-        return (count);
+	return (count);
 }
 
 /**
