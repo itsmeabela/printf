c645cc4e1780c2e60dd6022e08ba508ecaea3c33
ShagizMan <danielgetaneh2011@gmail.com>
2022-04-16 21:01:19 +0300

    modification on main

--- main.h
+++ main.h
@@ -14,7 +14,7 @@ typedef struct specifier
 	char *valid;
 	int (*f)(va_list);
 }spec;
-
+int (*get_func(char x))(va_list args);
 int _printf(const char *format, ...);
 int print_c(va_list args);
 int print_s(va_list args);
