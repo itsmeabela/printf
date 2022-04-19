bed2e108b34becf555526d853f8478e7ec520fa2
ShagizMan <danielgetaneh2011@gmail.com>
2022-04-19 14:10:30 +0300

    merge to one file _putchar

--- _putchar.c
+++ _putchar.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <unistd.h>
 
 /**
@@ -9,5 +10,38 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static char buf[1024];
+	static int index;
+
+	if (c == -1 || index >= 1024)
+	{
+		write(1, &buf, index);
+		index = 0;
+	}
+
+	if (c != -1)
+	{
+		buf[index] = c;
+		index++;
+	}
+
+	return (1);
+}
+
+/**
+ * _puts - Prints a string to stdout.
+ *
+ * @str: Pointer to the string to print.
+ *
+ * Return: Number of chars written.
+ */
+int _puts(char *str)
+{
+	register int index;
+
+	for (index = 0; str[index] != '\0'; index++)
+		_putchar(str[index]);
+
+	return (index);
+
 }
